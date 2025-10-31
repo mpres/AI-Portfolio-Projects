57180d5e747d0b98cd9b639d19b383776d50cf1e
mpres <michael.p.presley@gmail.com>
2025-10-31 20:10:17 +0000

    commit_message

--- Movie-Recommendation-System/Movie_Recommendation_System.py
+++ Movie-Recommendation-System/Movie_Recommendation_System.py
@@ -2,6 +2,7 @@ import os
 import requests
 from pathlib import Path
 import zipfile
+import pandas as pd
 
 
 def explore_dir(path):
@@ -9,3 +10,7 @@ def explore_dir(path):
   for dir_path, dir_names, file_names in os.walk(path):
     print(f"Directory count {len(dir_names)}, movies count {len(file_names)} in '{file_names}'")
 
+
+### 10/31/25 grab files and merge them into a useful structure, one file using pandas
+
+!pwd
