9845021f9a03c506bc42cb45be1e17a59bff326c
mpres <michael.p.presley@gmail.com>
2025-11-17 22:10:59 +0000

    commit_message

--- Movie-Recommendation-System/MR_functions.py
+++ Movie-Recommendation-System/MR_functions.py
@@ -92,14 +92,73 @@ def get_titles(movie_df: pd.DataFrame,ids: list) -> list:
     returns movies titles
   '''
   try:
-    
+
     assert 'movieId' in movie_df.columns
     assert 'title' in movie_df.columns
     return movies_df[movies_df['movieId'].isin(ids)]
 
-    
+
 
   except Exception as e:
     print(f'An error occurred')
-  
+
   return ""
+
+#added create_new_user function for gradio 11/17/25
+def create_new_user(df: pd.DataFrame, new_user_id: int, user_movie_scores: list[tuple[int,float]] ) -> pd.DataFrame:
+  '''
+    purpose:
+            This function will take in a new_user_id, a dataframe and user_movie_scores
+             It will return return a new dataframe that represent an instance of one movie review for that user.
+             This function will be used for a gradio setup
+    outline:
+            validate data types, make sure df argument has the right columns. make usre new_user_is is unique to df
+            Loop through user_movie_socres, grab genre informatiob from movie id, create entry row for each loop iteration.
+            return new rows of data.
+
+    args:
+        df - this is a dataframe where we get the genre information about the movies entered.
+        new_user_id - this will be a new user_id that is unique to the df frame colum user_Id.
+        user_movie_scores - this should be a list of tuples, the first value is the movie_Id the second is a rating of 0.0 - 5.0 the increment of .5
+    return:
+        returns rows in the same format as rows in the df pandas data frame.
+
+  '''
+
+  #Validate inputs
+
+  if not isinstance(df, pd.DataFrame):
+    raise TypeError("Input 'df' must be a pandas data frame")
+
+  if not isinstance(new_user_id, int):
+    raise TypeError("Input 'new_user_id' must be an int")
+
+  if not isinstance(user_movie_scores, list):
+    raise TypeError("Input 'user_movie_scores' must be a list of tuples with an int,float")
+
+  #Validate make sure df has user a column user
+
+  if not 'userId' in df.columns:
+    raise Exception("the 'userId' column is not in DataFrame 'df'")
+
+  if new_user_id in set(df['userId']):
+    raise Exception(f" the new_user_id {new_user_id} is not unique")
+
+  for movie_pair in user_movie_scores:
+    if not movie_pair[0] in list(df['movieId']):
+      raise Exception(f"the movie id {movie_pair[0]} is not found in the data frame 'df'")
+    if movie_pair[1] > 5 or movie_pair[1] < 0:
+      raise Exception(f"the movie rating {movie_pair[1]} for movie id {movie_pair[0]} must be between 0 and 5")
+
+  #capture current df into df_result 11/17/25
+  df_result = df.copy(deep=True)
+
+  #Loop through the user_movie_scores
+
+  for movie_pair in user_movie_scores:
+    row = df[df['movieId'] == movie_pair[0]].iloc[0:1]
+    row[['userId','rating','timestamp']] = new_user_id, movie_pair[1], time.time()
+    df_result = pd.concat([df_result,row], ignore_index=True)
+    
+
+  return df_result
